2a54f67677227b33177eae9472735060e6c1d465
An0n1mity <an0nimity@protonmail.com>
2022-10-04 16:47:39 +0200

    Hihihihi

--- Entity.c
+++ Entity.c
@@ -37,25 +37,6 @@ inline void setDirection(struct Entity_s *entity, enum Direction_e direction)
     entity->m_direction = direction;
 }
 
-struct TilemapBlock_s *getFrontTilemapBlock(struct Entity_s *entity, struct Tilemap_s *tilemap)
-{
-    assert(entity && tilemap);
-
-    int entity_x = entity->m_position.m_x,
-        entity_y = entity->m_position.m_y;
-
-    if (entity->m_direction == NORTH && (entity_y - 1 >= 0))
-        return tilemap->m_array[entity_y * tilemap->m_width + entity_x];
-    else if (entity->m_direction == SOUTH && (entity_y + 1 < tilemap->m_height))
-        return tilemap->m_array[entity_y + tilemap->m_width + entity_x];
-    else if (entity->m_direction == WEST && (entity_x - 1 >= 0))
-        return tilemap->m_array[entity_y + tilemap->m_width + entity_x];
-    else if (entity->m_direction == EAST && (entity_x + 1 < tilemap->m_width))
-        return tilemap->m_array[entity_y + tilemap->m_width + entity_x];
-    else
-        return NULL;
-}
-
 struct Block_s *getFrontBlock(struct Entity_s *entity, struct Tilemap_s *tilemap)
 {
     assert(entity && tilemap);
